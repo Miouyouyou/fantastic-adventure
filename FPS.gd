a2f2abe42333860dfd391b036c54f08d48590c7a
Miouyouyou (Myy) <myy@miouyouyou.fr>
2020-12-22 09:36:15 +0100

    Godot animator is just wonderful
    The animation took just a few lines of code, in order
    to convert the keyboard input to a joystick like input.
    
    And that's it. The animation blender in Godot
    "Just Works" for tihs project. And that's all I need
    for now.
    
    There's no crouch, nor jump handling for now.
    
    The next step will be OpenXR. There's already a flag
    inside the AnimationTree that freeze the head, arms
    and the hips in order to let the Inverse Kinematics
    handle them, when coupled with XR inputs.
    
    So the last setps are now :
    * Add the OpenXR plugin on Windows
    * Implement Inverse Kinematics to move the arms in a
      "plausible" way, so that the character doesn't get
      deformed when moving the arms and the head.
    * Compile an independant Windows and Linux binary
    * Change the model of the character, and its
      AnimationPlayer with another model that has the same
      animations names, instantiated using a GLB downloaded
      on the network.
    
    That said, if the last step doesn't really work
    that well, it will still be OK. I'll release the
    0.0alpha1 in that shape.
    
    %aybe just in time for the holidays ?
    
    Signed-off-by: Miouyouyou (Myy) <myy@miouyouyou.fr>

--- FPS.gd
+++ FPS.gd
@@ -13,11 +13,7 @@ var velocity = Vector3()
 var fall = Vector3()
 
 onready var head = $Head
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var animation_tree = $AnimationTree
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -45,24 +41,37 @@ func _input(event):
 func _physics_process(delta):
 
 	direction = Vector3()
+	var joystick:Vector2 = Vector2()
 
 	if not is_on_floor():
 		fall.y -= gravity * delta
 	elif Input.is_action_just_pressed("jump"):
 		fall.y = jump
 
-	if Input.is_action_pressed("move_forward"):
-		direction -= transform.basis.z
-	elif Input.is_action_pressed("move_backwards"):
-		direction += transform.basis.z
+	var joy2:Vector2 = Vector2(
+		(Input.get_action_strength("move_right") * 1.0) +
+		(Input.get_action_strength("move_left") * -1.0),
+		(Input.get_action_strength("move_forward") * 1.0)
+		+ (Input.get_action_strength("move_backwards") * -1.0))
 
-	if Input.is_action_pressed("move_left"):
-		direction -= transform.basis.x
-	elif Input.is_action_pressed("move_right"):
-		direction += transform.basis.x
+	direction += (transform.basis.x) * joy2.x
+	direction += (transform.basis.z) * (-joy2.y)
+	#if Input.is_action_pressed("move_forward"):
+	#	direction -= transform.basis.z
+	#	joystick.y += 1.0
+	#elif Input.is_action_pressed("move_backwards"):
+	#	direction += transform.basis.z
+	#	joystick.y -= 1.0
 
+	#if Input.is_action_pressed("move_left"):
+	#	direction -= transform.basis.x
+	#	joystick.x -= 1.0
+	#elif Input.is_action_pressed("move_right"):
+	#	direction += transform.basis.x
+	#	joystick.x += 1.0
+
+	animation_tree.set("parameters/move/blend_position", joy2)
 	direction = direction.normalized()
 	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
 	velocity = move_and_slide(velocity, Vector3.UP)
-	move_and_slide(fall, Vector3.UP)
-
+	var move:Vector3 = move_and_slide(fall, Vector3.UP)
