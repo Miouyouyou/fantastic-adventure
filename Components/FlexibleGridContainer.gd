2ff465ab52c3620fa4415c9307befea4e1c0e006
Miouyouyou (Myy) <myy@miouyouyou.fr>
2021-03-27 03:10:59 +0100

    Potential new helpers for the UI
    Signed-off-by: Miouyouyou (Myy) <myy@miouyouyou.fr>

---
+++ Components/FlexibleGridContainer.gd
@@ -0,0 +1,15 @@
+extends ItemList
+
+func _ready():
+	var p = preload("res://UI/ModelThumbnail.tscn")
+	var i = p.instance()
+	add_child(i)
+
+	var imagetex_read:ImageHelpers.StatusAndImageTexture = ImageHelpers.read_image_texture_from_disk("/tmp/test.png")
+	if imagetex_read.status == OK:
+		print("YAY !")
+		i.set_thumbnail(imagetex_read.image)
+		i.set_description("Meow Meow")
+	else:
+		print_debug(imagetex_read.status)
+	return
